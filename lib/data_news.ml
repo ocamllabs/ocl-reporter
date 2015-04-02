cba4e887d44e1e9e1a402ce581eabdd39258eb29
Anil Madhavapeddy <anil@recoil.org>
2015-04-02 17:57:41 +0100

    add 2015 yearly.
    still editing in pictures on my personal site copy of the post

--- lib/data_news.ml
+++ lib/data_news.ml
@@ -3,6 +3,7 @@ let mk_date y m =
   (y,m,1,12,0), (Printf.sprintf "%.4d-%.2d-%.2d" y m 1)
 
 let monthlies = [
+  (2015,04,02,12,0), (Printf.sprintf "2015-04-02");
   (2013,12,29,12,0), (Printf.sprintf "2013-12-29");
   mk_date 2013 6;
   mk_date 2013 5;
